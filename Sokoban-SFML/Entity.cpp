b86b4e4bd10c404747f5a4f5cc7f3a402e2109e1
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-11-07 00:14:15 +0300

    Moved calls of move() method from Entity::update() to GameState::handleInput()

--- Sokoban-SFML/Entity.cpp
+++ Sokoban-SFML/Entity.cpp
@@ -15,18 +15,7 @@ void Entity::move(const sf::Time deltaTime, const float dir_x, const float dir_y
 }
 
 void Entity::update(const sf::Time deltaTime) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-		move(deltaTime, -1.f, 0.f);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-		move(deltaTime, 1.f, 0.f);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-		move(deltaTime, 0.f, -1.f);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-		move(deltaTime, 0.f, 1.f);
-	}
+	
 }
 
 void Entity::render(sf::RenderWindow* window) {
--- Sokoban-SFML/GameState.cpp
+++ Sokoban-SFML/GameState.cpp
@@ -12,7 +12,18 @@ GameState::~GameState() {
 
 
 void GameState::handleInput(const sf::Time deltaTime) {
-
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+		_player.move(deltaTime, -1.f, 0.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+		_player.move(deltaTime, 1.f, 0.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+		_player.move(deltaTime, 0.f, -1.f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+		_player.move(deltaTime, 0.f, 1.f);
+	}
 }
 
 void GameState::update(const sf::Time deltaTime) {
