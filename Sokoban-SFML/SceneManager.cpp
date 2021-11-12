f8564b44fbf473ade3fd758afa615d6296c84a42
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-11-12 19:38:36 +0300

    Added texture map and texture managment methods to SceneManager

--- Sokoban-SFML/SceneManager.cpp
+++ Sokoban-SFML/SceneManager.cpp
@@ -1,1 +1,23 @@
 #include "SceneManager.hpp"
+
+SceneManager::SceneManager() :
+	_textures()
+{
+	
+}
+
+SceneManager::~SceneManager() {
+
+}
+
+void SceneManager::AddTexture(const std::string& name, const std::string& filePath, bool repeat) {
+	sf::Texture* texture = new sf::Texture();
+	texture->loadFromFile(filePath);
+	texture->setRepeated(repeat);
+	_textures.insert(std::make_pair(name, texture));
+}
+
+sf::Texture* SceneManager::getTexture(const std::string& name) const {
+	auto it = _textures.find(name);
+	return (it != _textures.end()) ? it->second : nullptr;
+}
--- Sokoban-SFML/SceneManager.hpp
+++ Sokoban-SFML/SceneManager.hpp
@@ -1,5 +1,18 @@
 #pragma once
-class SceneManager
-{
+
+#include <map>
+#include <string>
+#include <SFML/Graphics.hpp>
+
+class SceneManager {
+private:
+	std::map<std::string, sf::Texture*> _textures;
+public:
+	SceneManager();
+	~SceneManager();
+
+	void AddTexture(const std::string& name, const std::string& filePath, bool isRepeated = false);
+
+	sf::Texture* getTexture(const std::string& name) const;
 };
 
