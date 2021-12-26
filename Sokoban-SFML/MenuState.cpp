cf4e5bca48c99023349ad07944df56f47875cf70
Adaypon <75831434+Adaypon@users.noreply.github.com>
2021-12-26 17:17:41 +0300

    Beta version message display in MenuState

--- Sokoban-SFML/MenuState.cpp
+++ Sokoban-SFML/MenuState.cpp
@@ -16,7 +16,7 @@ void MenuState::init() {
 	_context->_assets->AddFont("Main font", "assets/fonts/font.ttf");
 	_text.setFont(_context->_assets->getFont("Main font"));
 	_text.setCharacterSize(42);
-	_text.setString("Alpha ver. Press Enter to start\nPress ESC to quit\nPress TAB for settings");
+	_text.setString("Beta ver. Press Enter to start\nPress ESC to quit\nPress TAB for settings");
 	_text.setOrigin(_text.getLocalBounds().width / 2, _text.getLocalBounds().height / 2);
 	_text.setPosition(_context->_window->getView().getSize().x / 2, _context->_window->getView().getSize().y / 2 + 150);
 
